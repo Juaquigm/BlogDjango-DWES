77516dc733cfde8e87df44406fa24cd768e7c948
Juaquigm <jmorongo@gmail.com>
2019-11-05 23:02:51 +0100

    Subida - Listado de Posts

--- miSitio/Blog/views.py
+++ miSitio/Blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'Blog/post_list.html',{})
+    posts = Post.objects.filter(fecha_publicada__lte=timezone.now()).order_by('fecha_publicada')
+    return render(request, 'Blog/post_list.html',{'posts':posts})
